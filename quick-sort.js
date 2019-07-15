adddaa59cd936f8678a5ab64e3015a3438384ee7
zhaozhipeng <zhipeng.zhao@cloudminds.com>
2019-07-15 10:41:16 +0800

    best quick sort

--- quick-sort.js
+++ quick-sort.js
@@ -1,47 +1,39 @@
 const utils = require('./utils');
-function fastSort(arr) {
-    function sort(arr, left, right) {
-        if (left >= right) {
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    const quickSort = function(arr, start, end){
+        if(start >= end){
             return;
         }
-        let i = left
-        let j = right
-        let len = arr.length;
-        let baseIndex = j;
-        let base = arr[baseIndex];
-        while (i < j) {
-
-            while (i < j && i < len) {
-                if (arr[i] > base) {
-                    let tmp = arr[i];
-                    arr[i] = arr[baseIndex];
-                    arr[baseIndex] = tmp;
-                    baseIndex = i;
-                    break;
-                }
+        
+        let i = start, base = arr[end];
+        for(let j = start; j <= end; j++){
+            if(arr[j] < base){
+                let tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
                 i++;
             }
-
-            while (j >= 0 && j > i) {
-                if (arr[j] < base) {
-                    let tmp = arr[j];
-                    arr[j] = base;
-                    arr[baseIndex] = tmp;
-                    baseIndex = j;
-                    break;
-                }
-                j--;
-            }
         }
-
-        sort(arr, left, baseIndex - 1);
-        sort(arr, baseIndex + 1, right);
+        
+        //keep pivot in end
+        let tmp = arr[i];
+        arr[i] = arr[end];
+        arr[end] = tmp;
+        
+        quickSort(arr, start, i - 1);
+        quickSort(arr, i + 1, end);
     }
-    sort(arr, 0, arr.length - 1);
-    return arr;
-}
-
-let arr = utils.makeIntegerArray(200000, 0, 10);
+    
+    quickSort(nums, 0, nums.length - 1);
+    return nums;
+};
+let arr = utils.makeIntegerArray(2000, 0, 10);
 console.time('run')
-fastSort(arr);
+
+sortArray(arr);
+console.log(arr)
 console.timeEnd('run')
